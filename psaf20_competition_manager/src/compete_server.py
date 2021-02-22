547f28276cd4ac82d9d04977406345fad985375b
Lennart Luttkus <lennart.luttkus@uni-a.de>
2021-02-22 11:25:49 +0100

    minor improvements to server, but not helpful

--- psaf20_competition_manager/src/compete_server.py
+++ psaf20_competition_manager/src/compete_server.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python3
 
+"""
+http://wiki.ros.org/actionlib_tutorials/Tutorials/Writing%20a%20Simple%20Action%20Server%20using%20the%20Execute%20Callback%20%28Python%29
+
+"""
+
 import rospy
 
 import actionlib
@@ -12,7 +17,11 @@ class compete_action(object):
 
     def __init__(self, name):
         self._action_name = name
-        self._as = actionlib.SimpleActionServer(self._action_name, psaf20_competition_manager.CompeteAction, execute_cb=self.execute_cb, auto_start=False)
+        self._as = actionlib.SimpleActionServer(
+            self._action_name, 
+            psaf20_competition_manager.CompeteAction, 
+            execute_cb=self.execute_cb, 
+            auto_start=False)
         self._as.start()
 
     def execute_cb(self, goal):
@@ -20,7 +29,12 @@ class compete_action(object):
         r = rospy.Rate(1)
         success = True
 
+        # publish info to the console for the user
         rospy.loginfo('%s: Executing.' % (self._action_name))
+        
 
 
-
+if __name__ == '__main__':
+    rospy.init_node('compete')
+    server = compete_action(rospy.get_name())
+    rospy.spin()
