4c8cd5a6a2f14ded30b64776b1edefdf7589d3db
Lennart Luttkus <lennart.luttkus@uni-a.de>
2021-02-22 11:27:08 +0100

    extended to timing

--- psaf20_competition_manager/src/simple_competition_manager.py
+++ psaf20_competition_manager/src/simple_competition_manager.py
@@ -20,15 +20,38 @@ def callback(data):
         y=rospy.get_param('competition/goal/position/y'),
         z=rospy.get_param('competition/goal/position/z'))
     distance_to_goal = get_distance(current_postion, goal_position)
-    rospy.loginfo('distance to goal: {}'.format(distance_to_goal))
     
     start_position = Point(
         x=rospy.get_param('competition/start/position/x'),
         y=rospy.get_param('competition/start/position/y'),
         z=rospy.get_param('competition/start/position/z'))
     distance_to_start = get_distance(current_postion, start_position)
-    rospy.loginfo('distance to start: {}'.format(distance_to_start))
+
+    if not rospy.get_param('competition/finished'):
+        rospy.loginfo('distance to goal: {}'.format(distance_to_goal))
+        rospy.loginfo('distance to start: {}'.format(distance_to_start))
     
+    if not rospy.get_param('competition/started'):
+        if distance_to_start > 0.5:
+            rospy.set_param('competition/started', True)
+            rospy.set_param(
+                'competition/start_time', rospy.get_rostime().to_sec())
+            rospy.logwarn(
+                'competition started at: {}'.format(
+                    rospy.get_param('/competition/start_time')))
+
+    if rospy.get_param('competition/started') and not rospy.get_param('competition/finished'):
+        if distance_to_goal < 1.0:
+            rospy.set_param('competition/finished', True)
+            rospy.set_param(
+                'competition/finish_time', rospy.get_rostime().to_sec())
+            duration = (rospy.get_param('competition/finish_time')
+                - rospy.get_param('competition/start_time'))
+            rospy.set_param('competition/duration', duration)
+            rospy.logwarn(
+                'The competition was completed in {} seconds'.format(duration))
+
+
 
 def competition_manager():
     rospy.init_node('competion_manager')
@@ -37,4 +60,6 @@ def competition_manager():
     
 if __name__ == '__main__':
     print('Init competion_manager')
+    rospy.set_param('competition/started', False)
+    rospy.set_param('competition/finished', False)
     competition_manager()
