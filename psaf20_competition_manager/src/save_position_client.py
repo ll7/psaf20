d3d0627a5302105bf111e6d4a4e2d3c5ae55eabc
Lennart Luttkus <lennart.luttkus@uni-a.de>
2021-02-22 11:26:48 +0100

    layout modified

--- psaf20_competition_manager/src/save_position_client.py
+++ psaf20_competition_manager/src/save_position_client.py
@@ -3,7 +3,6 @@
 import rospy
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import NavSatFix
-from rosgraph_msgs.msg import Clock
 
 from psaf20_competition_manager.srv import SavePosition
 
@@ -12,7 +11,10 @@ def save_position_client(odom):
 
     try:
         safe_position = rospy.ServiceProxy('save_position', SavePosition)
-        saved = safe_position(save = True, position = odom, GPS = NavSatFix())
+        saved = safe_position(
+            save = True, 
+            position = odom, 
+            GPS = NavSatFix())
         return saved
     except rospy.ServiceException as e:
         print("Service call failed: %s" %e)
@@ -22,7 +24,10 @@ if __name__ == '__main__':
     print("Requesting save_position")
     rospy.init_node('listen_to_odom')
     try:
-        odom = rospy.wait_for_message("carla/ego_vehicle/odometry", Odometry, timeout=1)
+        odom = rospy.wait_for_message(
+            "carla/ego_vehicle/odometry", 
+            Odometry, 
+            timeout=1)
     except rospy.ROSException as e:
         odom = Odometry()
         print("wait_for_message failed: %s" %e)
